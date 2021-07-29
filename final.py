e0377a73d3613db1023d124867598aa891ed0c01
benschulman <bgschulman31@gmail.com>
2021-07-29 01:41:53 -0500

    updated final.py to reflect retrieve/load/calculate paradigm

--- final.py
+++ final.py
@@ -4,11 +4,11 @@ Authors: Henry Singer, Ryan Loutos, Ben Schulman
 '''
 
 # projected war functions
-from predictions.war_functions.get_active_roster import get_all_active_rosters
-from predictions.war_functions.get_pecota_tables import get_combined_pecota_table
-from predictions.war_functions.get_previous_year_war import get_combined_previous_year_war_table
-from predictions.war_functions.get_overall_war_predictions import get_overall_war_predictions
-from predictions.war_functions.get_final_war_table import get_final_war_table
+from predictions.war_functions.active_rosters import load_active_rosters, retrieve_all_active_rosters
+from predictions.war_functions.pecota_tables import load_combined_pecota_table
+from predictions.war_functions.war_table import retrieve_historical_combined_war_table, load_combined_war_table
+from predictions.war_functions.war_projections import calculate_team_war_projections_table
+from predictions.war_functions.final_war_table import calculate_final_war_table
 
 # cluster luck functions
 from predictions.cluster_luck_functions.get_cluster_luck_hitting import get_cluster_luck_hitting_table
@@ -20,13 +20,30 @@ from predictions.get_final_win_percentage_table import get_win_percentage_predic
 from player_adjustments.get_starting_rotations import get_starting_rotations
 from player_adjustments.get_todays_games_info import get_todays_games_info
 
+import datetime as dt
+
+curr_year = dt.date.today().year
+last_year = curr_year - 1
+
+# ------------- WAR -------------
+# Retrieve Data
+retrieve = True
+if retrieve:
+    retrieve_all_active_rosters()
+    retrieve_historical_combined_war_table(last_year, file_path=f"data/war_table_{last_year}csv")
+
+# Load Data
 # projected war based off of last year and pecota
-active_rosters = get_all_active_rosters()
-pecota_table = get_combined_pecota_table()
-previous_year_war_table = get_combined_previous_year_war_table()
-projected_war_table = get_overall_war_predictions(active_rosters, pecota_table)
-final_war_table = get_final_war_table(
-    projected_war_table, previous_year_war_table)
+active_rosters = load_active_rosters()
+pecota_table = load_combined_pecota_table()
+previous_year_war_table = load_combined_war_table(file_path=f"data/war_table_{last_year}.csv")
+
+# Calculate Results
+projected_war_table = calculate_team_war_projections_table(active_rosters, pecota_table)
+final_war_table = calculate_final_war_table(projected_war_table, previous_year_war_table)
+
+
+# ------------- Cluster Luck -------------
 
 # cluster luck tables
 cluster_luck_hitting_table = get_cluster_luck_hitting_table()
