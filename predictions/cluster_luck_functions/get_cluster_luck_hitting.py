e24249f0f7691f2bc5787367c041caac5c263baf
benschulman <bgschulman31@gmail.com>
2021-08-03 14:42:04 -0500

    refactored cluster_luck hitting file to follow ret/load/calc

--- predictions/cluster_luck_functions/get_cluster_luck_hitting.py
+++ predictions/cluster_luck_functions/get_cluster_luck_hitting.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import datetime as dt
 import pickle
 
 
@@ -59,6 +58,9 @@ def _retrieve_single_year_hitting_table(year: int) -> pd.DataFrame:
     combined_stats_table = pd.concat([stats_table_html[0], stats_table_html[1]], axis=1)
     hitting_table = hitting_table.append(combined_stats_table)
 
+    # Convert Team names to standard naming convention
+    hitting_table.Team = hitting_table.Team.apply(lambda x: _team_map[x])
+
     return hitting_table
 
 def retrieve_historical_hitting_tables(years: list[int], file_name="data/historical_team_hitting.csv") -> pd.DataFrame:
@@ -96,7 +98,15 @@ def retrieve_historical_hitting_tables(years: list[int], file_name="data/histori
     return multi_year_hitting_table
 
 def load_historical_hitting_tables(file_name="data/historical_team_hitting.csv") -> pd.DataFrame:
-    pass
+    """Loads a hitting table from a saved file.
+
+    Args:
+        file_name (str, optional): Path to load data from. Defaults to "data/historical_team_hitting.csv".
+
+    Returns:
+        pd.DataFrame: Historical hitting table
+    """
+    return pd.read_csv(file_name, index_col=0)
 
 def calculate_and_save_hitting_linear_regression(previous_three_years_table: pd.DataFrame, file_name='./data/hitting_regression.pickle')->LinearRegression:
     """Runs a linear regression on Hits per Run based on team's previous years OBP, ISO and SLG.
@@ -130,66 +140,30 @@ def calculate_and_save_hitting_linear_regression(previous_three_years_table: pd.
 
     return linear_regression
 
-def get_prev_year_hitting_table(linear_regression):
-    # TODO get prev_year automatically
-    prev_year = dt.date.today().year - 1
-    # pull the html for last years stats
+def calculate_predicted_cluster_luck_run_adjustment_hitting(linear_regression:LinearRegression, hitting_data:pd.DataFrame)->pd.DataFrame:
+    """Constructs a DataFrame with the predicted HPR and the run adjustment based on a linear regression
+    and hitting data.
 
-    url_last_year ='https://www.espn.com/mlb/stats/team/_/stat/batting/season/%s/seasontype/2' % prev_year
-    prev_year_hitting_html = pd.read_html(url_last_year)
+    Args:
+        linear_regression (LinearRegression): A linear regression that results from `calculate_and_save_hitting_linear_regression`
+        hitting_data (pd.DataFrame): a hitting data table (usually from previous year) resulting from `retrieve_historical_hitting_tables(last_year)`
 
-    # combine the tables accordingly
-    prev_year_hitting = pd.concat(
-        [prev_year_hitting_html[0], prev_year_hitting_html[1]], axis=1)
+    Returns:
+        pd.DataFrame: A DataFrame with the predicted HPR per team and cluster_luck run hitting adjustment per team
+    """
+    # Don't want to change original DataFrame so Make a copy for results
+    prev_year_hitting = hitting_data.copy()
 
-    # add stats columns
-    prev_year_hitting['ISO'] = (prev_year_hitting['2B']+2*prev_year_hitting['3B'] +
-                                3*prev_year_hitting['HR']) / prev_year_hitting['AB']
-    prev_year_hitting['HPR'] = prev_year_hitting['H'] / prev_year_hitting['R']
-
-    # predict hits per run from regression
+    # The X variables in the regression
     regression_x_vars = prev_year_hitting.loc[:, ['OBP', 'ISO', 'SLG']]
+    # The prediction based on the regression
     prev_year_hitting['predict'] = linear_regression.predict(regression_x_vars)
 
-    # add a run adjustment column based on prediction
-    prev_year_hitting['run_adjust'] = (
-        (prev_year_hitting['HPR'] - prev_year_hitting['predict']) / prev_year_hitting['HPR'])*prev_year_hitting['R']
-
-    # add a team name row accordingly
-    # team_list = ['Astros', 'Twins', 'Red Sox', 'Yankees', 'Rockies', 'Nationals', 'Pirates', 'White Sox', 'Braves', 'Dodgers', 'Mets', 'Rays', 'Cubs', 'Diamondbacks',
-    #              'Indians', 'Athletics', 'Rangers', 'Angels', 'Royals', 'Brewers', 'Orioles', 'Phillies', 'Cardinals', 'Reds', 'Marlins', 'Tigers', 'Giants', 'Padres', 'Mariners', 'Blue Jays']
-    # prev_year_hitting['Team'] = team_list
-
-    prev_year_hitting.Team = prev_year_hitting.Team.apply(lambda x: _team_map[x])
+    # Calculate the run adjustment column based on prediction
+    prev_year_hitting['run_adjust'] = ( (prev_year_hitting['HPR'] - prev_year_hitting['predict']) / prev_year_hitting['HPR']) * prev_year_hitting['R']
 
     return prev_year_hitting
 
-
-def get_cluster_luck_hitting_table():
-    previous_three_years_hitting_table = get_prev_three_years_hitting_table()
-    linear_regression_hitting = get_hitting_linear_regression(
-        previous_three_years_hitting_table)
-    cluster_luck_hitting_table = get_prev_year_hitting_table(
-        linear_regression_hitting)
-
-    return cluster_luck_hitting_table
-
-def get_cluster_luck_hitting_current_year():
-    current_year = dt.date.today().year
-    url_this_year = 'https://www.espn.com/mlb/stats/team/_/stat/batting/season/%s/seasontype/2' % current_year
-    current_year_html = pd.read_html(url_this_year)
-    current_year_hitting = pd.concat(
-        [current_year_html[0], current_year_html[1]], axis=1)
-    current_year_hitting['ISO'] = (current_year_hitting['2B']+2*current_year_hitting['3B'] +
-                                3*current_year_hitting['HR']) / current_year_hitting['AB']
-    current_year_hitting['HPR'] = current_year_hitting['H'] / current_year_hitting['R']
-    regression = get_hitting_linear_regression(get_prev_three_years_hitting_table())
-    regression_x_vars = current_year_hitting.loc[:, ['OBP', 'ISO', 'SLG']]
-    current_year_hitting['predict'] = regression.predict(regression_x_vars)
-    current_year_hitting['run_adjust'] = (
-        (current_year_hitting['HPR'] - current_year_hitting['predict']) / current_year_hitting['HPR'])*current_year_hitting['R']
-    current_year_hitting.Team = current_year_hitting.Team.apply(lambda x: _team_map[x])
-    return current_year_hitting
     
 
 
