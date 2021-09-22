02b7be4aeaf5298b38de2ca83835799e32b7cb17
Henry Singer <hsinger24@Henrys-MBP-2.attlocal.net>
2021-09-22 13:43:09 -0500

    Daily runs last few days

--- sandbox.py
+++ sandbox.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 ########## DELETING BETS ##########
-bets_today = pd.read_csv('past_bets/base/bets_20210919.csv', index_col = 0)
-bets_today.drop([9, 13], axis = 0, inplace = True)
+bets_today = pd.read_csv('past_bets/base/bets_20210922.csv', index_col = 0)
+bets_today.drop([1,], axis = 0, inplace = True)
 bets_today.reset_index(inplace = True, drop = True)
-bets_today.to_csv('past_bets/base/bets_20210919.csv')
+bets_today.to_csv('past_bets/base/bets_20210922.csv')
