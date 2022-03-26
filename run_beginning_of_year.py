869c017e2abb37e7833088b09e25cf5e89209731
Henry Singer <henrysinger24@gmail.com>
2022-03-26 09:09:22 -0500

    BOY cluster luck functions audit first pass

--- run_beginning_of_year.py
+++ run_beginning_of_year.py
@@ -24,25 +24,25 @@ rosters = load_active_rosters()
 
 # # Getting cluster luck hitting #
 
-# retrieve_historical_hitting_tables([2019, 2018, 2017])
+retrieve_historical_hitting_tables([2021, 2019, 2018, 2017])
 historical_hitting_table = load_historical_hitting_tables()
-# calculate_and_save_hitting_linear_regression(historical_hitting_table)
+calculate_and_save_hitting_linear_regression(historical_hitting_table)
 hitting_reg = load_linear_regression("data/hitting_regression.pickle")
-prev_year_hitting_table = retrieve_historical_hitting_tables(2019)
+prev_year_hitting_table = retrieve_historical_hitting_tables(2021, file_name = None)
 prev_year_hitting_adjustment = calculate_predicted_cluster_luck_run_adjustment_hitting(hitting_reg, prev_year_hitting_table)
 
 # # Getting cluster luck pitching #
 
-# retrieve_historical_pitching_tables([2019, 2018, 2017])
+retrieve_historical_pitching_tables([2021, 2019, 2018, 2017])
 historical_pitching_table = load_historical_pitching_tables()
-# calculate_and_save_pitching_linear_regression(historical_pitching_table)
+calculate_and_save_pitching_linear_regression(historical_pitching_table)
 pitching_reg = load_linear_regression('./data/pitching_regression.pickle')
-prev_year_pitching_table = retrieve_historical_pitching_tables(2019)
+prev_year_pitching_table = retrieve_historical_pitching_tables(2021, file_name = None)
 prev_year_pitching_adjustment = calculate_predicted_cluster_luck_run_adjustment_pitching(pitching_reg, prev_year_pitching_table)
 
 # # Merging #
 
-final_cluster_luck = merge_cluster_luck_tables(prev_year_hitting_adjustment, prev_year_pitching_adjustment)
+final_cluster_luck = merge_cluster_luck_tables(prev_year_hitting_adjustment, prev_year_pitching_adjustment, file_name = 'data/final_cluster_luck.csv')
 
 ########## WAR FUNCTIONS ##########
 
